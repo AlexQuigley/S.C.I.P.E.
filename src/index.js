51984b4726616969762418e1bcb37fe90f623676
AlexQuigley <alexander.quigley2001@gmail.com>
2024-09-15 13:45:14 -0500

    Changes Added discord.js Installed nodemon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 //Tutorial: https://www.youtube.com/watch?v=KZ3tIGHU314
-
 const {Client, IntentsBitField} = require('discord.js');
 
 //New instance of bot
-const client = new Client({
+const SCIPE = new Client({
     //Intents = Permissions for your bot (https://discord.com/developers/docs/topics/gateway)
+    //Manual (1.01)
     intents: [
-        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.Guilds, 
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
     ]
-}) 
+});
+
+SCIPE.login(token); //Passes the bots token ("password") KEEP SECRET
+//type: 'node src/index.js' in the cmd prompt to run bot
+
